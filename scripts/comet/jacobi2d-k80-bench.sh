59669b2e2f0b48343c815adcc0f4f3459f70cbb1
Jaemin Choi <jchoi157@comet-ln3.sdsc.edu>
2019-10-09 19:24:22 -0700

    Update jacobi2d scripts on Comet

--- scripts/comet/jacobi2d-k80-bench.sh
+++ scripts/comet/jacobi2d-k80-bench.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:k80:4
 #SBATCH --ntasks-per-node=24
 #SBATCH --nodes=8
-#SBATCH --job-name="jacobi2d-n32"
+#SBATCH --job-name="jacobi2d-k80-n32"
 #SBATCH -t 00:00:10
 
 date
@@ -16,5 +16,5 @@ block_size=16384
 for iter in 1 2 3
 do
   echo "Running iteration $iter"
-  mpiexec -N 4 ./jacobi2d-b -s $block_size -i 100 > jacobi2d-n"$ranks"-"$iter".out
+  mpiexec -N 4 ./jacobi2d-b -s $block_size -i 100 > jacobi2d-k80-n"$ranks"-"$iter".out
 done
