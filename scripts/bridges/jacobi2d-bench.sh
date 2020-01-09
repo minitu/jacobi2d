476d07958fa0842f77184ccacf14bcbaad3a962a
Jaemin Choi <jchoi157@illinois.edu>
2020-01-08 23:34:59 -0500

    Add separate Makefile for Bridges, update benchmarking script on Bridges

--- scripts/bridges/jacobi2d-bench.sh
+++ scripts/bridges/jacobi2d-bench.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -p GPU
-#SBATCH --nodes=2
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=16
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:p100:2
-#SBATCH --time=00:30:00
-#SBATCH --job-name=jacobi2d-n4
+#SBATCH --time=01:00:00
+#SBATCH --job-name=jacobi2d-n6
 
 date
 
-cd /g/g90/choi18/jacobi2d
+cd /home/jchoi157/jacobi2d
 
+ranks=6
 block_size=16384
 
 export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
@@ -18,5 +18,5 @@ export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
 for iter in 1 2 3
 do
   echo "Running iteration $iter"
-  mpiexec -print-rank-map -n $SLURM_NTASKS -genv I_MPI_DEBUG=5 ./jacobi2d-b -s $block_size -i 100 > jacobi2d-n"$SLURM_NTASKS"-"$iter".out
+  mpiexec -print-rank-map -n $ranks -ppn 2 -genv I_MPI_DEBUG=5 ./jacobi2d-b -s $block_size -i 100 > jacobi2d-n"$ranks"-"$iter".out
 done
