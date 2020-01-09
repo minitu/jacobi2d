d58ae80aac5fdc2b61b956bc25d3d321172c6bc0
Jaemin Choi <jchoi157@illinois.edu>
2020-01-08 21:02:46 -0800

    Update MPI-replay script to match HPM structure

--- replay.sh
+++ replay.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dir="summit-maxrate"
-block_size=16384
+machine="summit"
+dir="$machine-replay"
 
 #for rank in 6
 for rank in 6 12 24 48 96 192 384 768 1536
 do
   echo "Running $rank ranks"
-  jsrun -n20 -a1 -c1 -K10 -r20 $HOME/work/codes-dumpi/build/src/network-workloads/model-net-mpi-replay --sync=3 --disable_compute=0 --workload_type="dumpi" --workload_file=/ccs/home/jchoi/work/jacobi2d/dumpi/s"$block_size"/n"$rank"- --num_net_traces="$rank" --lp-io-dir="$dir"/s"$block_size"/n"$rank" -- "$dir"/summit-replay.conf &> "$dir"/s"$block_size"/n"$rank".out
+  jsrun -n20 -a1 -c1 -K10 -r20 $HPM_PATH/codes/build/src/network-workloads/model-net-mpi-replay --sync=3 --disable_compute=0 --workload_type="dumpi" --workload_file=$HPM_PATH/apps/jacobi2d/summit-dumpi/s16384/n"$rank"- --num_net_traces="$rank" --lp-io-dir="$dir"/n"$rank" -- $HPM_PATH/conf/$machine/replay.conf &> "$dir"/n"$rank".out
 done
